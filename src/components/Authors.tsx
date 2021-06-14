2470fcaea7fa1ed098725749e3d34bf6bf6efe47
sriyabro <sriyabro@gmail.com>
2021-06-14 08:41:00 +0530

    add items component added

--- src/components/Authors.tsx
+++ src/components/Authors.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import {Row} from "react-bootstrap";
 import Header from "./Header";
 import NoItemsLabel from "./NoItemsLabel";
+import AddItem from "./AddItem";
 
 const Authors: React.FC = () => {
     return (
         <Row className="authors">
             <Header header="Authors" />
             <NoItemsLabel message={"No authors listed here"} />
+            <AddItem item={"Author"} />
         </Row>
     );
 }
