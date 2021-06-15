982bb12ac2a7c093921696156073e8267cc82d83
sriyabro <sriyabro@gmail.com>
2021-06-16 04:39:23 +0530

    author form completed

--- src/components/author/AddAuthor.tsx
+++ src/components/author/AddAuthor.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Col } from 'react-bootstrap';
-import { Plus } from "react-feather"
+import {Col} from 'react-bootstrap';
+import {Plus} from "react-feather"
 
 const AddAuthor: React.FC = () => {
     return (
-            <Col xs={12} className="add-author mx-0 my-3 p-0">
+            <Col xs={12} className="add-author mx-0 my-2 p-0">
                 <h6>
                     <Plus size={24} className="plus-icon p-0"/>
                     <span className="pl-2 py-0">Add Author</span>
