e688f5d3d81ce7db94574b6a1af2b715e225d588
sriyabro <sriyabro@gmail.com>
2021-06-16 05:38:36 +0530

    adding author form

--- src/components/author/AuthorForm.tsx
+++ src/components/author/AuthorForm.tsx
@@ -1,11 +1,82 @@
 import React from "react";
-import { Col } from 'react-bootstrap';
+import {Button, Col, Form, InputGroup} from 'react-bootstrap';
 
 const AuthorForm: React.FC = () => {
     return (
-            <Col xs={12} className="author-form">
-
-            </Col>
+        <Col xs={12} lg={{span: 10, offset: 2}} className="author-form">
+            <Form noValidate validated={true} >
+                <Form.Row>
+                    <Form.Group as={Col} md="4" controlId="validationCustom01">
+                        <Form.Label>First name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="First name"
+                            defaultValue="Mark"
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="4" controlId="validationCustom02">
+                        <Form.Label>Last name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="Last name"
+                            defaultValue="Otto"
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                        <Form.Label>Username</Form.Label>
+                        <InputGroup hasValidation>
+                            <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <Form.Control
+                                type="text"
+                                placeholder="Username"
+                                aria-describedby="inputGroupPrepend"
+                                required
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                Please choose a username.
+                            </Form.Control.Feedback>
+                        </InputGroup>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                    <Form.Group as={Col} md="6" controlId="validationCustom03">
+                        <Form.Label>City</Form.Label>
+                        <Form.Control type="text" placeholder="City" required/>
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid city.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="3" controlId="validationCustom04">
+                        <Form.Label>State</Form.Label>
+                        <Form.Control type="text" placeholder="State" required/>
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid state.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="3" controlId="validationCustom05">
+                        <Form.Label>Zip</Form.Label>
+                        <Form.Control type="text" placeholder="Zip" required/>
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid zip.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Group>
+                    <Form.Check
+                        required
+                        label="Agree to terms and conditions"
+                        feedback="You must agree before submitting."
+                    />
+                </Form.Group>
+                <Button type="submit">Submit form</Button>
+            </Form>
+        </Col>
     );
 }
 
