7746fc89ce22f20157328ed44803c3c1b60a99b0
sriyabro <sriyabro@gmail.com>
2021-06-16 11:58:11 +0530

    Form inputs added

--- src/components/book/BookForm.tsx
+++ src/components/book/BookForm.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import {Col, Row} from "react-bootstrap";
-import {XCircle} from "react-feather";
+import React, {useState} from "react";
+import { Col, Form, Row } from "react-bootstrap";
+import { XCircle } from "react-feather";
+import FormButton from "../FormButton";
 
 const BookForm: React.FC = () => {
+    const [formValidate, setFormValidate] = useState<boolean>(false);
     return (
         <Col xs={12} lg={9} className="form mt-4 px-0">
             <Col xs={12}>
@@ -16,7 +18,19 @@ const BookForm: React.FC = () => {
                 </Row>
             </Col>
             <Col xs={12} lg={{span: 11, offset: 1}} className="px-0">
-
+                <Form noValidate validated={formValidate} >
+                        <Form.Group className="mb-2">
+                            <Form.Label className="mb-0 ml-1">Title of the Book</Form.Label>
+                            <Form.Control size="sm" type="text" required />
+                            <Form.Control.Feedback type="invalid">Please enter the title of the book</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group className="mb-2">
+                            <Form.Label className=" mb-0 ml-1">ISBN</Form.Label>
+                            <Form.Control size="sm" type="text" required />
+                            <Form.Control.Feedback type="invalid">Please enter a valid ISBN</Form.Control.Feedback>
+                        </Form.Group>
+                    <FormButton updateClicked={false} />
+                </Form>
             </Col>
         </Col>
     );
