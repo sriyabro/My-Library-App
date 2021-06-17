05dce23c278fb1061308d602af08858cfaadd04a
sriyabro <sriyabro@gmail.com>
2021-06-17 09:30:19 +0530

    IBook type added

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -2,6 +2,12 @@ export interface IAuthor {
     name: string
 }
 
+export interface IBook {
+    name: string
+    ISBN: string
+    author: IAuthor
+}
+
 export interface IAlert {
     message : string
     variant : string
