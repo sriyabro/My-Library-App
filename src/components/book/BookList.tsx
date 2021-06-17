918be5850b498ff91c80c34b3a85f18e94b0b154
sriyabro <sriyabro@gmail.com>
2021-06-17 08:34:47 +0530

    Selector options added

--- src/components/book/BookList.tsx
+++ src/components/book/BookList.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { Col, ListGroup } from "react-bootstrap";
 import Book from "./Book";
+import {IBook} from "../../types/Types";
 
-const BookList: React.FC = () => {
+type BookListProps = {
+    books: IBook[] | null
+}
+
+const BookList: React.FC<BookListProps> = (props) => {
+    const {books} = props;
+
     return (
         <Col xs={12}>
-            <ListGroup className="mx-0">
-                <ListGroup.Item className="p-0 border-0">
-                    <Book />
-                </ListGroup.Item>
-            </ListGroup>
+            {books && <ListGroup className="mx-0">
+                {books.map((book: IBook, index: number ) => {
+                    return (
+                        <ListGroup.Item className="p-0 border-0" key={index}>
+                            <Book key={index}
+                                  book={book}
+                                  index={index}
+                            />
+                        </ListGroup.Item>
+                    );
+                })
+                }
+            </ListGroup>}
         </Col>
     );
 }
