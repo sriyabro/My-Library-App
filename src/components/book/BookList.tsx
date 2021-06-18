3fe7a3fc9983781ba7b674a23f5eae4783a120af
sriyabro <sriyabro@gmail.com>
2021-06-18 08:11:56 +0530

    update book done

--- src/components/book/BookList.tsx
+++ src/components/book/BookList.tsx
@@ -4,21 +4,23 @@ import Book from "./Book";
 import {IBook} from "../../types/Types";
 
 type BookListProps = {
-    books: IBook[] | null
+    books: IBook[] | null,
+    onUpdateClick: (index: number) => void,
 }
 
 const BookList: React.FC<BookListProps> = (props) => {
-    const {books} = props;
+    const {books, onUpdateClick} = props;
 
     return (
         <Col xs={12}>
-            {books && <ListGroup className="mx-0">
+            {books && <ListGroup className="mx-0 mt-4">
                 {books.map((book: IBook, index: number ) => {
                     return (
                         <ListGroup.Item className="p-0 border-0" key={index}>
                             <Book key={index}
                                   book={book}
                                   index={index}
+                                  onUpdateClick={onUpdateClick}
                             />
                         </ListGroup.Item>
                     );
