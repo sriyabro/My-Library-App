06219beea3a855f98e63e27dd5561d502087c290
sriyabro <sriyabro@gmail.com>
2021-06-18 08:42:36 +0530

    delete book done

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -1,25 +1,31 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { Edit, Trash2 } from "react-feather";
+import {Col, Row} from "react-bootstrap";
+import {Edit, Trash2} from "react-feather";
 import {IBook} from "../../types/Types";
 
 type BookProps = {
     book: IBook,
     index: number,
     onUpdateClick: (index: number) => void
+    onDeleteClick: (index: number) => void
+
 }
 
 const Book: React.FC<BookProps> = (props) => {
-    const {book, index, onUpdateClick} = props;
+    const {book, index, onUpdateClick, onDeleteClick} = props;
 
     return (
         <Row className="item-row pt-2 pb-1">
             <Col xs={8} className="pl-0">
-                <h5>{index+1}. {book.name}</h5>
+                <h5>{index + 1}. {book.name}</h5>
             </Col>
             <Col xs={4} className="text-right px-0">
-                <Edit className="icon mx-2 text-warning" size={20} onClick={()=> {onUpdateClick(index)}}/>
-                <Trash2 className="icon mx-2 text-danger" size={20}/>
+                <Edit className="icon mx-2 text-warning" size={20} onClick={() => {
+                    onUpdateClick(index)
+                }}/>
+                <Trash2 className="icon mx-2 text-danger" size={20} onClick={() => {
+                    onDeleteClick(index)
+                }}/>
             </Col>
         </Row>
     );
