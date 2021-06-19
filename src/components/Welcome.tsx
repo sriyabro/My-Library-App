f7b9ce08114e4527ea89b88344f71c3965651c9a
sriyabro <sriyabro@gmail.com>
2021-06-19 17:56:25 +0530

    isbn formatted, price added with formatting

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,26 +3,26 @@ import {Col, Image, Row} from "react-bootstrap";
 import WelcomeImg from '../assets/images/welcome-library.jpg'
 
 const Welcome: React.FC = () => {
-    return (
-        <React.Fragment>
-            <Row className='welcome'>
-                <Col xs={12} className='text-center my-2'>
-                    <h1>My Library</h1>
-                </Col>
-                <Col xs={12} className='mx-0 px-0'>
-                    <Image src={WelcomeImg} />
-                </Col>
-                <Col xs={12} className='credits text-center text-md-right pr-md-5 pt-1'>
-                    Photo by <a href="https://unsplash.com/@annahunko?utm_source=unsplash&utm_medium=referral&utm_
+  return (
+    <React.Fragment>
+      <Row className='welcome'>
+        <Col xs={12} className='text-center my-2'>
+          <h1>My Library</h1>
+        </Col>
+        <Col xs={12} className='mx-0 px-0'>
+          <Image src={WelcomeImg} alt="Library Image"/>
+        </Col>
+        <Col xs={12} className='credits text-right pr-3 pr-md-5 pt-1'>
+          Photo by <a href="https://unsplash.com/@annahunko?utm_source=unsplash&utm_medium=referral&utm_
                     content=creditCopyText" rel="noreferrer" target="_blank">
-                    Anna Hunko </a>
-                    on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_
+          Anna Hunko </a>
+          on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_
                     content=creditCopyText" rel="noreferrer" target="_blank">
-                    Unsplash </a>
-                </Col>
-            </Row>
-        </React.Fragment>
-    );
+          Unsplash </a>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
 }
 
 export default Welcome;
