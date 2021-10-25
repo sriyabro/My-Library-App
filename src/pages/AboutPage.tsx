78d38da3ed3cdd6de2b82276ea22d7b66b008116
sriyabro <sriyabro@gmail.com>
2021-10-25 20:45:57 +0530

    about page added

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1>AboutPage works!</h1>
+    </React.Fragment>
+  );
+}
+
+export default AboutPage;
