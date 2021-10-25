915b59ccb42e030b8bb13d3339b310939e732851
sriyabro <sriyabro@gmail.com>
2021-10-25 21:39:05 +0530

    authors moved to authorsPage, removed from library body

--- src/components/author/AddAuthor.tsx
+++ src/components/author/AddAuthor.tsx
@@ -9,7 +9,7 @@ type AddAuthorProps = {
 const AddAuthor: React.FC<AddAuthorProps> = (props) => {
     const {addAuthorClicked} = props;
     return (
-            <Col xs={12} className="add-item mx-0 mt-1 mb-3 p-0">
+            <Col xs={12} className="add-item mx-0 mt-4 mb-3 py-2 px-0">
                 <h6 onClick={() => addAuthorClicked()}>
                     <Plus size={24} className="plus-icon p-0"/>
                     <span className="pl-2 py-0">Add Author</span>
