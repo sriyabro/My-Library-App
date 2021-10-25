0eeb336b9c71c1e3475b1d94ec4cbf2632bd0ea6
sriyabro <sriyabro@gmail.com>
2021-10-26 00:03:11 +0530

    about page updated

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Col, Row} from "react-bootstrap";
 import Header from "../components/Header";
+import {GitHub} from "react-feather";
 
 const AboutPage: React.FC = () => {
   return (
     <React.Fragment>
       <Row className="about m-3 p-0">
-        <Col xs={12} md={6} className="p-0 p-md-2">
+        <Col xs={12} className="p-0 p-md-2">
           <Header header="About"/>
+        </Col>
+        <Col className="m-4 d-flex align-items-center">
+          <GitHub size={30}/>
+          <a href="https://github.com/sriyabro/My-Library-App" target="_blank"><span className="h2 text-body">&nbsp;&nbsp;My Library App</span></a>
         </Col>
       </Row>
     </React.Fragment>
