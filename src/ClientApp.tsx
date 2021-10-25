fe96a06d1272347656076d6d3ba2031665575f01
sriyabro <sriyabro@gmail.com>
2021-10-25 18:44:26 +0530

    routing done

--- src/ClientApp.tsx
+++ src/ClientApp.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Col, Container, Row} from "react-bootstrap";
-import Welcome from "./components/Welcome";
-import LibraryBody from "./components/LibraryBody";
+import HomePage from "./pages/HomePage";
+import BooksPage from "./pages/BooksPage";
+import AuthorsPage from "./pages/AuthorsPage";
 
 const ClientApp: React.FC = () => {
-    return (
-        <Container fluid={true}>
-            <Row>
-                <Col xs={12} className='text-center my-2'>
-                    <h1 className="header-nav">My Library</h1>
-                </Col>
-                <Col xs={12} >
-                    <Welcome />
-                </Col>
-                <Col xs={12} >
-                    <LibraryBody />
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container fluid={true}>
+      <Row>
+        <Col xs={12} className='text-center my-2'>
+          <h1 className="header-nav">My Library</h1>
+        </Col>
+        <Router>
+          <Switch>
+            <Route path="/authors">
+              <AuthorsPage/>
+            </Route>
+            <Route path="/books">
+              <BooksPage/>
+            </Route>
+            <Route path="/">
+              <HomePage/>
+            </Route>
+          </Switch>
+        </Router>
+      </Row>
+    </Container>
+  );
 }
 
 export default ClientApp;
