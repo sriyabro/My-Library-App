daec930513ce6802ea67330b970dcd035ae7766c
sriyabro <sriyabro@gmail.com>
2021-10-25 21:39:05 +0530

    authors moved to authorsPage, removed from library body

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import {Col} from "react-bootstrap";
 import Welcome from "../components/Welcome";
-import LibraryBody from "../components/LibraryBody";
 
 const HomePage: React.FC = () => {
   return (
     <React.Fragment>
       <Col xs={12}>
         <Welcome/>
-      </Col>
-      <Col xs={12}>
-        <LibraryBody/>
       </Col>
     </React.Fragment>
   );
