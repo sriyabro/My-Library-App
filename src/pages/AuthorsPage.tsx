e1f719655913a74a3bc5674b1b424b2998c7b9f2
sriyabro <sriyabro@gmail.com>
2021-10-25 18:44:04 +0530

    pages added

---
+++ src/pages/AuthorsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthorsPage: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1>AuthorsPage works!</h1>
+    </React.Fragment>
+  );
+}
+
+export default AuthorsPage;
